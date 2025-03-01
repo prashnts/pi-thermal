70d4b1f55415180b2e8ff0c32f5618912fb0d569
Prashant Sinha <prashant@noop.pw>
2025-03-01 15:35:49 +0100

    initial commit

--- main.py
+++ main.py
@@ -1,6 +1,33 @@
-def main():
-    print("Hello from pi-thermal!")
+import time
+import os
+import subprocess
 
+from watchdog.events import FileSystemEvent, FileSystemEventHandler, FileCreatedEvent, FileModifiedEvent
+from watchdog.observers import Observer
+
+
+CAN_PRINT = os.path.exists("./PRODUCTION")
+
+class EventHandler(FileSystemEventHandler):
+    def on_any_event(self, event: FileSystemEvent) -> None:
+        match event:
+            case FileModifiedEvent(_):
+                print(f"Printing: {event.src_path}")
+                if CAN_PRINT:
+                    subprocess.run(["lp", event.src_path])
+            case _:
+                pass
 
 if __name__ == "__main__":
-    main()
+    os.makedirs("./inbox", exist_ok=True)
+
+    event_handler = EventHandler()
+    observer = Observer()
+    observer.schedule(event_handler, "./inbox", recursive=True)
+    observer.start()
+    try:
+        while True:
+            time.sleep(1)
+    finally:
+        observer.stop()
+        observer.join()
