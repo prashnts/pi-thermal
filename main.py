35284a7e7eecaa159be68631c869b2e74e728d09
Prashant Sinha <prashant@noop.pw>
2025-03-01 15:52:32 +0100

    initial commit

--- main.py
+++ main.py
@@ -1,19 +1,38 @@
 import time
 import os
 import subprocess
-
+import functools
+from threading import Timer
 from watchdog.events import FileSystemEvent, FileSystemEventHandler, FileCreatedEvent, FileModifiedEvent
 from watchdog.observers import Observer
 
 
 CAN_PRINT = os.path.exists("./PRODUCTION")
 
+
+def debounce(timeout: float):
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            wrapper.func.cancel()
+            wrapper.func = Timer(timeout, func, args, kwargs)
+            wrapper.func.start()
+        
+        wrapper.func = Timer(timeout, lambda: None)
+        return wrapper
+    return decorator
+
+
+@debounce(1)
+def print_file(path: str):
+    print(f"Printing: {path}")
+    if CAN_PRINT:
+        subprocess.run(["lp", path])
+
 class EventHandler(FileSystemEventHandler):
     def on_any_event(self, event: FileSystemEvent) -> None:
         if isinstance(event, FileModifiedEvent):
-            print(f"Printing: {event.src_path}")
-            if CAN_PRINT:
-                subprocess.run(["lp", event.src_path])
+            print_file(event.src_path)
 
 if __name__ == "__main__":
     os.makedirs("./inbox", exist_ok=True)
