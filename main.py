e0252aac1ca6f26ba3e765c6bcd07d13e3359a58
Prashant Sinha <prashant@noop.pw>
2025-03-29 23:55:33 +0100

    Update main.py

--- main.py
+++ main.py
@@ -34,6 +34,20 @@ class EventHandler(FileSystemEventHandler):
         if isinstance(event, FileModifiedEvent):
             print_file(event.src_path)
 
+@debounce(5)
+def screensaver():
+    result = subprocess.run(['w'], stdout=subprocess.PIPE)
+    stdout = result.stdout.decode()
+    for line in stdout.splitlines():
+        if ':console' not in line:
+            continue
+        if 's' not in line.split('\t')[4]:
+            print("saving screen")
+            if 'bash /u' not in line:
+                subprocess.run(["screen", "-x myTTY1", '-X stuff "pipes.sh^M"'])
+                print("saved")
+
+
 if __name__ == "__main__":
     os.makedirs("./inbox", exist_ok=True)
 
@@ -43,6 +57,7 @@ if __name__ == "__main__":
     observer.start()
     try:
         while True:
+            screensaver()
             time.sleep(1)
     finally:
         observer.stop()
