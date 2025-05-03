2f655331e0c9fb13cf093a5c62fc8ef2c7589667
Prashant Sinha <prashant@noop.pw>
2025-05-03 18:41:29 +0200

    fix size

--- labels.py
+++ labels.py
@@ -2,14 +2,14 @@ from uuid import uuid4
 from ppf.datamatrix import DataMatrix
 import fpdf
 
-pdf = fpdf.FPDF(format=(50, 80))
+pdf = fpdf.FPDF(format=(58, 80))
 pdf.add_page()
 
 for y in range(8):
     for x in range(5):
         dm = DataMatrix(f"it-{uuid4()}")
         svg = fpdf.svg.SVGObject(dm.svg())
-        svg.transform_to_rect_viewport(1, 8, 8)
-        svg.draw_to_page(pdf, x * 10, y * 10)
+        svg.transform_to_rect_viewport(1, 12, 12)
+        svg.draw_to_page(pdf, x * 12, y * 12)
 
 pdf.output('inbox/labels.pdf')
